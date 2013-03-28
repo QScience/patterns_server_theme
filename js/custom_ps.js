bfa0e8650350d147204c645fc320bee1a72074f3
ray <xrfind@gmail.com>
2013-03-28 02:02:39 +0100

    rate to liked

--- js/custom_ps.js
+++ js/custom_ps.js
@@ -2,28 +2,28 @@ jQuery(document).ready(function() {
 	jQuery('.breadcrumb').css('display', 'none');
 	jQuery('.page-header').css('display', 'none');
 
-	//rate
-	var rate_link = jQuery(".pattern_row .rate-link");
-	jQuery.each( rate_link , function(ix,ele){
+	//like
+	var liked_link = jQuery(".pattern_row .liked-link");
+	jQuery.each( liked_link , function(ix,ele){
 		if (jQuery(ele).text() == "Unlike") {
-			jQuery(ele).addClass('rate-link-t');
+			jQuery(ele).addClass('liked-link-t');
 		}
 	});
-    rate_link.click(function() {
+    liked_link.click(function() {
 		if (jQuery(this).text() == "Like") {
 			jQuery(this).text("Unlike");
 			//jQuery(this).css( 'background-image', 'url("../img/unlike01.png" )');
-			jQuery(this).addClass('rate-link-t');
-			var rate_times = jQuery(this).parent().prev().prev();
-			var numb = Number(rate_times.text()) + 1; 
-			rate_times.children().text(numb);
+			jQuery(this).addClass('liked-link-t');
+			var liked_times = jQuery(this).parent().prev().prev();
+			var numb = Number(liked_times.text()) + 1; 
+			liked_times.children().text(numb);
 		}
 		else {
 			jQuery(this).text("Like");
-			jQuery(this).removeClass('rate-link-t');
-			var rate_times = jQuery(this).parent().prev().prev();
-			var numb = Number(rate_times.text()) - 1; 
-			rate_times.children().text(numb);
+			jQuery(this).removeClass('liked-link-t');
+			var liked_times = jQuery(this).parent().prev().prev();
+			var numb = Number(liked_times.text()) - 1; 
+			liked_times.children().text(numb);
 		}
 
         var url_div = jQuery(this).attr("href");
